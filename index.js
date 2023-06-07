269a9ecbd6677eb04b6315ed58df36d64ea15829
Apu Sutra Dhar <apusutradhar77@gmail.com>
2023-06-07 16:04:08 +0600

    created a basic express server

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+require("dotenv").config();
+var jwt = require("jsonwebtoken");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+// ????///////////
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
